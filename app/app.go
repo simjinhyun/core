928a13662b08b94c1c5f0c966288d2bc8305c783
simjinhyun <simjinhyun1972@gmail.com>
2025-12-19 17:01:19 +0000

    초기커밋

---
+++ app/app.go
@@ -0,0 +1,58 @@
+package app
+
+import (
+	"context"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+type App struct {
+	Initialize     func() error
+	RequestHandler func(http.ResponseWriter, *http.Request)
+	Finalize       func() error
+	server         *http.Server
+}
+
+func New(addr string, init func() error, handler func(http.ResponseWriter, *http.Request), finalize func() error) *App {
+	return &App{
+		Initialize:     init,
+		RequestHandler: handler,
+		Finalize:       finalize,
+		server: &http.Server{
+			Addr:    addr,
+			Handler: http.HandlerFunc(handler),
+		},
+	}
+}
+
+func (a *App) Run() {
+	// 1. 초기화 콜백 실행
+	if a.Initialize != nil {
+		if err := a.Initialize(); err != nil {
+			panic(err)
+		}
+	}
+
+	// 2. 서버 실행
+	go func() {
+		if err := a.server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			panic(err)
+		}
+	}()
+
+	// 3. 종료 시그널 대기 후 Finalize 콜백 실행
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, syscall.SIGINT, syscall.SIGTERM)
+	<-stop
+
+	if a.Finalize != nil {
+		_ = a.Finalize()
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	_ = a.server.Shutdown(ctx)
+}
